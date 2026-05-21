43c4705709c10ebd5bdc237156f6b6b6628b54a6
dennyvibeware <denny@unoeste.edu.br>
2026-05-21 05:34:48 +0000

    xadrez.c - desafio nivel mestre finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,50 +1,72 @@
 #include <stdio.h>
 
+void movimento_da_torre(int x){
+    if(x > 0){
+        printf("[Direita] ");
+        movimento_da_torre(x-1);
+    }
+}
+
+void movimento_do_bispo(int x){
+
+    if(x > 0){
+        for(int i = 0; i < 1; i++){
+            printf("[Cima ");
+            for(int j = 0; j < 1; j++){
+                printf("Direita] ");
+            }
+        }
+        movimento_do_bispo(x-1);
+    }
+}
+
+void movimento_da_rainha(int x){
+    if(x > 0){
+        printf("[Esquerda] ");
+        movimento_da_rainha(x-1);
+    }
+}
+
+// 2 casas para cima e 1 para direita
+// no desafio nao foi pedido recursividade
+void movimento_do_cavalo(){ 
+
+    //usar loop aninhados
+    //usar multiplas variaveis e/ou condicoes
+    //usar continue e break
+    int i = 1, j = 1;
+    while(i--){
+        for(j = 0; j < 2; j++){
+            printf("[Cima] ");
+        }
+        printf("[Direita] \n");
+    }
+}
+
 int main(){
 
-    int i=1, j=1; // contador
     int casas_torre = 5, casas_bispo = 5, casas_rainha = 8, casas_cavalo=3;
 
     //movimento da torre
     printf("Movimentos da torre");
     printf("\n===================\n");
-    for(i=0; i < casas_torre; i++) // 0 1 2 3 4 , a variavel i precisa começar de 0 para repetir 5x
-    {
-        printf("[Direita] ");
-    }
+    movimento_da_torre(casas_torre);
 
     //movimento do bispo
     printf("\n\nMovimentos do bispo");
     printf("\n===================\n");
-    i = 0;
-    while(i < casas_bispo){
-        printf("[cima e direita] ");
-        i++;
-    }
+    movimento_do_bispo(casas_bispo);
 
-    //movimento da rainha
+    // movimento da rainha
     printf("\n\nMovimentos da rainha");
     printf("\n====================\n");
-    i = 0;
-    do{
-        printf("[Esquerda] ");
-        i++;
-    } while(i < casas_rainha);
-    
+    movimento_da_rainha(casas_rainha);
+
     //movimento do cavalo
     printf("\n\nMovimentos do cavalo");
     printf("\n====================\n");
-    i=1;
-    while(i--){
-        for(j = 0; j < 2; j++){
-            printf("[Baixo] ");
-        }
-        printf("[Esquerda] \n");
-    }
-        
-
-
+    movimento_do_cavalo();
+   
     return 0;
-
 
 }
